b03ff751b179085a0422652697fda45ade25605f
Yuvraj <yuvraj.gwps@gmail.com>
2022-11-20 10:20:04 +0530

    build setup

---
+++ build/precache-manifest.febdce1b9b6319b14c5409735bf24a37.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36432150f4264068d6ebd340d150ee73",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93771ec2c6de8027888a",
+    "url": "/static/css/main.59851a23.chunk.css"
+  },
+  {
+    "revision": "9931b4af5c8cd8e6aca6",
+    "url": "/static/js/2.8c7e2214.chunk.js"
+  },
+  {
+    "revision": "eca373d6688a5d5efead34244f081e8b",
+    "url": "/static/js/2.8c7e2214.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93771ec2c6de8027888a",
+    "url": "/static/js/main.2d2f8a31.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.2d2f8a31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05fbb5ed970a924e23b7",
+    "url": "/static/js/runtime-main.81534a39.js"
+  },
+  {
+    "revision": "37f89f938b1fc2470ec4cb469b1c57bf",
+    "url": "/static/media/BSocial.37f89f93.png"
+  }
+]);
